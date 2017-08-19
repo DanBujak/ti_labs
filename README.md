8b98a6b96a171538d1305e209079023285015b28
Daniel <bujak.dan@gmail.com>
2017-08-19 16:27:13 -0400

    Adding CC1350 Empty Project
    Removed CCS workspace and only including projects in GIT

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ti_labs
+
+http://dev.ti.com/tirex
+http://dev.ti.com/tirex/#/DevTool/CC1350%20LaunchPad/?link=
+http://dev.ti.com/tirex/content/simplelink_cc13x0_sdk_1_00_00_13/docs/proprietary-rf/html/cc13x0/index.html
--- empty_CC1350_LAUNCHXL_tirtos_ccs/empty.c
+++ empty_CC1350_LAUNCHXL_tirtos_ccs/empty.c
@@ -41,10 +41,11 @@
 
 /* Driver Header files */
 #include <ti/drivers/GPIO.h>
+#include <ti/drivers/ADC.h>
 // #include <ti/drivers/I2C.h>
 // #include <ti/drivers/SDSPI.h>
 // #include <ti/drivers/SPI.h>
-// #include <ti/drivers/UART.h>
+#include <ti/drivers/UART.h>
 // #include <ti/drivers/Watchdog.h>
 
 /* Board Header file */
@@ -55,22 +56,66 @@
  */
 void *mainThread(void *arg0)
 {
+    / ADC
+    ADC_Handle adc;
+    ADC_Params params;
+
+    // UART
+    char        input;
+    UART_Handle uart;
+    UART_Params uartParams;
+
     /* 1 second delay */
     uint32_t time = 1;
 
     /* Call driver init functions */
     GPIO_init();
+    ADC_Params_init(&params);
     // I2C_init();
     // SDSPI_init();
     // SPI_init();
-    // UART_init();
+    UART_init();
     // Watchdog_init();
 
     /* Turn on user LED */
-    GPIO_write(Board_GPIO_LED0, Board_GPIO_LED_ON);
+    GPIO_write(Board_GPIO_RLED, Board_GPIO_LED_ON);
+    GPIO_write(Board_GPIO_GLED, Board_GPIO_LED_OFF);
+
+    // Open ADC
+    adc = ADC_open(Board_ADC0, &params);
+    if (adc == NULL) {
+        // ADC_open() failed
+        while (1);
+    }
+
+    // Create a UART with data processing off.
+    UART_Params_init(&uartParams);
+    uartParams.writeDataMode = UART_DATA_BINARY;
+    uartParams.readDataMode = UART_DATA_BINARY;
+    uartParams.readReturnMode = UART_RETURN_FULL;
+    uartParams.readEcho = UART_ECHO_OFF;
+    uartParams.baudRate = 9600;
+
+    // Open an instance of the UART drivers
+    uart = UART_open(Board_UART0, &uartParams);
+
+    if (uart == NULL) {
+        // UART_open() failed
+        while (1);
+    }
 
     while (1) {
         sleep(time);
-        GPIO_toggle(Board_GPIO_LED0);
+        int_fast16_t res;
+        uint_fast16_t adcValue;
+
+        res = ADC_convert(adc, &adcValue);
+        if (res == ADC_STATUS_SUCCESS) {
+            if (adcValue > 100) {
+                GPIO_toggle(Board_GPIO_RLED);
+            }
+        }
+
+        GPIO_toggle(Board_GPIO_GLED);
     }
 }
